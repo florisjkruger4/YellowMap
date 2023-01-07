93b2e7cd0594347d64eb77aef08c5bd8751d827e
Floris Kruger <florisjkruger4@gmail.com>
2023-01-06 19:12:00 -0600

    added requirements.txt

--- yellowmap/settings.py
+++ yellowmap/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3hxlyz+(w5n0@nchasd3hsv!b-zgnt@q=a&so%k6&=0f+bwvq+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.floriskruger.com', 'yellowmap-production.up.railway.app/', '127.0.0.1']
 
 
 # Application definition
@@ -110,13 +110,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 GOOGLE_MAPS_API_KEY = "AIzaSyCLk3IekhFPUjvkYv5SGESs5eByDc35R0U"
+
+
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
