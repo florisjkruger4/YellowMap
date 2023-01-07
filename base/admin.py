83c7cf48232ef89170fe338cd8dabed34c2cb6ab
Floris Kruger <florisjkruger4@gmail.com>
2023-01-06 19:24:39 -0600

    import errors, debug still true

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django_google_maps import widgets as map_widgets
-from django_google_maps import fields as map_fields
 from .models import Post
 
 # Register your models here.
